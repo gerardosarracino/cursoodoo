6f720dd45fcfa30ca3a56d0831a3f5f50f8f6b5d
gerardosarracino <gerardo.sarracino.s@gmail.com>
2019-06-06 14:10:21 -0700

    creacion del modulo de prueba del curso

--- library/models/models.py
+++ library/models/models.py
@@ -3,20 +3,8 @@
 from odoo import models, fields, api
 
 
-class BibliotecaNombrelibro(models.Model):
-    _name = "biblioteca.nombrelibros"
+class LibraryBook(models.Model):
+    _name = "library.book"
 
-    name = fields.Char(string="Nombre")
-    descripcion = fields.Text(string="Descripcion")
-    precio = fields.Float(string="Precio")
-    ejemplares = fields.Integer(string="Ejemplares")
-    fecha = fields.Date(string="Fecha")
-    categoria = fields.Many2one("biblioteca.categoria", string="Categoria", required=True, ondelete="cascade")
-
-
-class BibliotecaCategoria(models.Model):
-    _name = "biblioteca.categoria"
-
-    name = fields.Char(string="Nombre", required=True)
-    descripcion = fields.Text(string="Descripcion")
-    libros = fields.One2many("biblioteca.nombrelibros", "categoria", string="Libros")
+    name = fields.Char(string="Book")
+    description = fields.Text(string="Description")
